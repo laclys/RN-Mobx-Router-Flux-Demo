34a275c3d2af78be03d942efd9324dc6abdafd6f
Lac <laclys@126.com>
2018-02-08 15:31:06 +0800

    add

--- reactMobx/components/Change.js
+++ reactMobx/components/Change.js
@@ -2,14 +2,33 @@ import React, {Component} from 'react'
 import {
   View,
   Text,
+  TouchableOpacity,
   StyleSheet
 } from 'react-native'
 
+import AppState from '../store/AppState'
+import {observer} from 'mobx-react'
+
+@observer
 export default class Change extends Component {
   render () {
     return (
       <View style={styles.container} >
-        <Text></Text>
+        <Text>name:{AppState.name}</Text>
+
+        <TouchableOpacity
+          style={{
+            marginTop: 50
+          }}
+          onPress={() => { AppState.changeName(AppState.name + 'さん') }}
+        >
+          <Text
+            style={{
+              color: 'red'
+            }}
+          >'さん'をつけてる</Text>
+        </TouchableOpacity>
+
       </View>
     )
   }
